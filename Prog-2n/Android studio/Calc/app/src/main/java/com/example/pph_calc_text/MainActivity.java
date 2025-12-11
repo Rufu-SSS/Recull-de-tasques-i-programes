1aa1d3b4d00c65686b97f264e51cf93f39c4f998
Rufu-SSS <polplanashoms@girona.lasalle.cat>
2025-12-11 12:11:35 +0100

    actualització amb nous fitxers i comentaris als codis d'android

--- Prog-2n/Android studio/Calc/app/src/main/java/com/example/pph_calc_text/MainActivity.java
+++ Prog-2n/Android studio/Calc/app/src/main/java/com/example/pph_calc_text/MainActivity.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,20 +14,25 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_sumar, btn_restar, btn_multiplicar, btn_dividir;
+    // Aquests botons els assignarem les operacions matemàtiques de suma, resta, multiplicació i divisió
     private TextView resultat;
+    // Mostrarem el resultat a través d'aquesta variable
     private EditText txt_num1, txt_num2;
-
+    // Tindrem dos camps de text editables els quals farem servir per escriure-hi els dos números
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        // Aprofitarem tota la pantalla del dispositiu android amb aquest mode Edge-to-Edge
+        
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Ajustarem els marges de l'aplicació per tal que no se solapin entre ells o quan la fem servir
 
         btn_sumar=findViewById(R.id.btn_suma);
         btn_restar=findViewById(R.id.btn_resta);
@@ -37,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         resultat=findViewById(R.id.txt_resultat);
         txt_num1=findViewById(R.id.txt_num1);
         txt_num2=findViewById(R.id.txt_num2);
+        // Assignació de components amb les seves respectives variables
 
         btn_sumar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +52,10 @@ public class MainActivity extends AppCompatActivity {
                 resultat.setText(String.valueOf(txt_resultat));
             }
         });
+        // El nostre botó de suma i els seus processos interns.
+        // Com busca els textos inserits a les variables num1 + num2 i com les suma 
+        // i retorna amb la variable resultat.
+
         btn_restar.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,6 +65,10 @@ public class MainActivity extends AppCompatActivity {
                 resultat.setText(String.valueOf(txt_resultat));
             }
         }));
+        // El nostre botó de resta i els seus processos interns.
+        // Com busca els textos inserits a les variables num1 - num2 i com les resta 
+        // i retorna amb la variable resultat.
+        
         btn_multiplicar.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,6 +78,10 @@ public class MainActivity extends AppCompatActivity {
                 resultat.setText(String.valueOf(txt_resultat));
             }
         }));
+        // El nostre botó de multiplicació i els seus processos interns.
+        // Com busca els textos inserits a les variables num1 * num2 i com les 
+        // multiplica i retorna amb la variable resultat.
+
         btn_dividir.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -74,12 +91,16 @@ public class MainActivity extends AppCompatActivity {
                 resultat.setText(String.valueOf(txt_resultat));
             }
         }));
-
+        // El nostre botó de divisió i els seus processos interns.
+        // Com busca els textos inserits a les variables num1 / num2 i com les  
+        // divideix i retorna amb la variable resultat.
     }
 
     public int sumar(int a, int b){return a+b;}
     public int restar(int a, int b){return a-b;}
     public int multiplicar(int a, int b){return a*b;}
     public int dividir(int a, int b){return a/b;}
-
+    // Aqui definim les funcions que ens permeten calcular els valors que posem als
+    // 'strings' (blocs de text) per després retornar-ne els seu resultat (de les variables
+    // que formen les operacions)
 }
--- Prog-2n/Android studio/Whatsapp 2/app/src/main/java/com/example/test/MainActivity.java
+++ Prog-2n/Android studio/Whatsapp 2/app/src/main/java/com/example/test/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.test;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,25 +12,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_act2;
-    Intent intent;
+    // Creem el botó que ens permetrà canviar d'activitat 1 a activitat 2
 
+    Intent intent;
+    // Intent ens permet iniciar una altra activitat
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        // Activem un estat el qual ens deixa aprofitar tota la pantalla
+
         setContentView(R.layout.activity_main);
+        // Carreguem el layout que associem a la segona activitat a partir del fitxer XML principal
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        // Ajustem els marges del contingut per tal que no se solapi res mentre es navega
 
         btn_act2 = findViewById(R.id.btn_act2);
+        // Assignem el nostre botó amb el layout base (activity 1)
+
         btn_act2.setOnClickListener(  View->{
             intent = new Intent(MainActivity.this, MainActivity2.class);
             startActivity(intent);
         });
+        // Diem al botó que ha de fer, en el nostre cas volem que vagi a la segona activitat, 
+        // per tant fem que el botó vagi endavant, tancant aixi l'activitat 1 per anar a la 2.
     }
 }
