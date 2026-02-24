c01649946fbe768d36ced8b0401f203737cd3809
Rufu-SSS <polplanashoms@girona.lasalle.cat>
2026-02-24 16:56:12 +0100

    si hi ha canvis, es pujen

--- Prog-2n/C# (Unity)/Joc2D-/Assets/PlayerMove.cs
+++ Prog-2n/C# (Unity)/Joc2D-/Assets/PlayerMove.cs
@@ -5,10 +5,23 @@ public class PlayerMove : MonoBehaviour
     public float speed = 10;
     private Rigidbody2D rigidbody2D;
     private Vector2 newVelocity;
+
+    public enum FACEDIRECTION { FACELEFT = -1, FACERIGHT = 1 };
+    public FACEDIRECTION Facing = FACEDIRECTION.FACERIGHT;
+
+    private void FlipDirection()
+    {
+        Facing = (FACEDIRECTION)((int)Facing * -1);
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
     private void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
         float xMove = Input.GetAxis("Horizontal");
@@ -17,9 +30,15 @@ public class PlayerMove : MonoBehaviour
         float ySpeed = yMove * speed;
         newVelocity = new Vector2(xSpeed, ySpeed);
     }
-    private void FixedUpdate()
+
+    void FixedUpdate()
     {
         rigidbody2D.linearVelocity = newVelocity;
-    }
 
+        if ((newVelocity.x < 0f && Facing != FACEDIRECTION.FACELEFT) ||
+            (newVelocity.x > 0f && Facing != FACEDIRECTION.FACERIGHT))
+        {
+            FlipDirection();
+        }
+    }
 }
